2e373db2705bda0bbd95540176906cdbbb637b8f
Lukas Güldenstein <git.lukasgueldenstein.de>
2025-05-04 14:32:39 +0200

    recompile exqlite

--- config/config.exs
+++ config/config.exs
@@ -39,6 +39,8 @@ config :esbuild,
     env: %{"NODE_PATH" => Path.expand("../deps", __DIR__)}
   ]
 
+config :exqlite, force_build: true
+
 # Configure tailwind (the version is required)
 config :tailwind,
   version: "3.4.3",
